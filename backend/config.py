0ef0d3e03523ee485ffa970355228658586455b1
Damilola Michael Ige <idm.calculus@gmail.com>
2023-10-17 03:33:19 +0100

    debuggin db not created issue

--- backend/config.py
+++ backend/config.py
@@ -5,7 +5,8 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', default='BAD_SECRET_KEY')
+
+    print(os.environ.get('DATABASE_URL'))
     
     if os.environ.get('DATABASE_URL'):
         SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
@@ -13,9 +14,6 @@ class Config(object):
         SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASEDIR, 'instance', 'rainfall_data.db')}"
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Logging
-    LOG_WITH_GUNICORN = os.environ.get('LOG_WITH_GUNICORN', default=False)
 
 class ProductionConfig(Config):
     DEBUG = False
